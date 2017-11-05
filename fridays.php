b08204d7cf4d80e8a73fa420c0e87eeb0bdbd5f5
Haris <blissgameryt@gmail.com>
2017-11-05 04:57:03 +0200

    fix

--- fridays.php
+++ fridays.php
@@ -18,17 +18,39 @@ class harisFridays {
 	public function run() {
 		$start = $this->y_start;
 		$end = $this->y_end;
-
+		
+		while ( ($end - $start + 1) > 400 ) {
+			$this->res += 167;
+			$end -= 400;
+		}
+		
+		
 		for( $i = $start; $i <= $end; $i++ ) {
-			if(date("L",mktime(0,0,0,1,1,$i))) {
+			if(checkdate(2, 29, $i)) {
 				for($month = 1; $month <= 12; $month++) {
+<<<<<<< HEAD
+					$date = new DateTime($i.'-'.$month.'-13');
+					
+	                if (5 == $date->format('w')) $this->res++;
+	            }
+=======
 					$date = mktime(0, 0, 0, $month, 13, $i);
 					if (5 == date('w', $date)) $this->res++;
 				}
+>>>>>>> 1beded7404e3937a3ad4a09094a3d5d44aa19fab
 			}
 		}
-
 		return $this->res;
 	}
 
 }
+<<<<<<< HEAD
+
+
+$start = microtime(true);
+$s = new harisFridays(1,100000004);
+echo $s->run();
+$time = microtime(true) - $start;
+printf('Скрипт выполнялся %.5F сек.\n', $time);
+=======
+>>>>>>> 1beded7404e3937a3ad4a09094a3d5d44aa19fab
