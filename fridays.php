8ac6af1e4ad5cefa1d6c1abcead7f94ad1157dcf
kmHarisUa <jail21@mail.ru>
2017-11-05 05:10:47 +0200

    fix

--- fridays.php
+++ fridays.php
@@ -28,29 +28,12 @@ class harisFridays {
 		for( $i = $start; $i <= $end; $i++ ) {
 			if(checkdate(2, 29, $i)) {
 				for($month = 1; $month <= 12; $month++) {
-<<<<<<< HEAD
 					$date = new DateTime($i.'-'.$month.'-13');
 					
 	                if (5 == $date->format('w')) $this->res++;
 	            }
-=======
-					$date = mktime(0, 0, 0, $month, 13, $i);
-					if (5 == date('w', $date)) $this->res++;
-				}
->>>>>>> 1beded7404e3937a3ad4a09094a3d5d44aa19fab
 			}
 		}
 		return $this->res;
 	}
-
 }
-<<<<<<< HEAD
-
-
-$start = microtime(true);
-$s = new harisFridays(1,100000004);
-echo $s->run();
-$time = microtime(true) - $start;
-printf('Скрипт выполнялся %.5F сек.\n', $time);
-=======
->>>>>>> 1beded7404e3937a3ad4a09094a3d5d44aa19fab
